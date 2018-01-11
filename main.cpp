a05a501320d8cb654e5cc93cc179d9c9d05c282f
Allison Nguyen <anguy129@.ucr.edu>
2018-01-11 13:43:04 -0800

    re-add branches

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main (){
-	string name;
-	cout << "What is your name?" << endl;
-	cin >> name;
-	cout << "Hello " << name << "!" << endl;
+	cout << "hello git" << endl;
 
 	return 0;
 }
